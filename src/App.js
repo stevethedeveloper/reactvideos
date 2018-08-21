7c94f7b97f97f74f1b2b588d0b91dbe92da806df
stevethedeveloper <info@stevethedeveloper.com>
2018-08-21 16:37:54 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import './App.css';
+import _ from 'lodash'
+import React, { Component } from 'react';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoDetail from './components/video_detail';
+import VideoList from './components/video_list';
+
+const API_KEY = 'AIzaSyAg-wxidgaOBLR0fK2SNPL6QAP1WEFSULc'
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        };
+
+        this.videoSearch('guitars');
+
+    }
+
+    videoSearch(term) {
+        YTSearch({key: API_KEY, term: term}, (videos) => {
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0]
+            });
+        });
+    }
+
+    render() {
+        const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
+
+        return (
+            <div>
+                <SearchBar onSearchTermChange={videoSearch} />
+                <VideoDetail video={this.state.selectedVideo} />
+                <VideoList
+                    onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                    videos={this.state.videos} />
+            </div>
+        );
+    }
+}
+
+export default App;
